50511dd4bd2b427b27c8a3d4414363237bbec20f
Zahlkovsk1 <148435929+Zahlkovsk1@users.noreply.github.com>
2023-11-08 12:26:50 +0100

    Simple Message

---
+++ boringDemo/boringDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  boringDemo
+//
+//  Created by SHOHJAHON on 08/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Boring Demo")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ boringDemo/boringDemo/boringDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  boringDemoApp.swift
+//  boringDemo
+//
+//  Created by SHOHJAHON on 08/11/23.
+//
+
+import SwiftUI
+
+@main
+struct boringDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
